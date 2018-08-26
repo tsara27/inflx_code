c575a528182d850819fc35388037dd95ca41dcc9
Tsara Fatma L <tsara.tf@gmail.com>
2018-08-26 16:18:57 +0700

    Add users and association with client

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,6 @@ class Client < ApplicationRecord
   STATUSES = [:green, :yellow, :red]
 
   enum status: STATUSES
+
+  has_many :users
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe User, type: :model do
+  # ==============
+  # Associations =
+  # ==============
+  it { should belong_to(:client) }
+end
